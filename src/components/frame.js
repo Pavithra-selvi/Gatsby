a52c6a15cc0b2462ea3f79c072e9c2fd7a94815f
Pavithra <pavithraaseervatham26@gmail.com>
2022-05-23 18:40:45 +0530

    finished tutorial gatsby

---
+++ src/components/frame.js
@@ -0,0 +1,10 @@
+import React from "react";
+const Frame = ({children}) =>{
+    return(
+        <div>
+            <h1>this is my first frame to use in my site</h1>
+            {children}
+        </div>
+    );
+}
+export default Frame;
---
+++ src/pages/gallery.js
@@ -0,0 +1,11 @@
+import React from "react";
+import Frame from "../components/frame";
+const Galleryimage = ({children}) =>{
+    return(
+          <Frame>
+          <h1>Hello world</h1>
+          </Frame>
+        
+    );
+}
+export default Galleryimage;
